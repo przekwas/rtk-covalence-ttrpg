0f196ff3d12c371f66dd060d07a4282ddfed5a17
Luke Przkwas <luke@covalence.io>
2022-08-20 01:47:58 -0500

    changes

--- src/components/Nav/StatCounter.jsx
+++ src/components/Nav/StatCounter.jsx
@@ -1,10 +1,14 @@
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const StatCounter = ({ name, select }) => {
+	const navigate = useNavigate();
 	const points = useSelector(state => state[select].points);
 
 	return (
-		<div>
+		<div
+			onClick={() => navigate(`/${select === 'skills' ? 'attributes' : select}`)}
+			className="cursor-pointer">
 			<span>{name}</span>{' '}
 			<span className={`font-bold tracking-widest ${!points.current && 'text-error'}`}>
 				{points.current}/{points.max}
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -7,15 +7,15 @@ const Home = () => {
 			<div
 				className="hero bg-base-200"
 				style={{ marginTop: '64px', minHeight: 'calc(100vh - 64px)' }}>
-				<div className="flex-col hero-content lg:flex-row-reverse">
+				<div className="flex-col text-center lg:text-left hero-content lg:flex-row-reverse">
 					<img src={CovalenceLogo} className="max-w-sm rounded-lg" alt="covalence logo" />
 					<div>
-						<h1 className="text-5xl font-bold text-blue-500">Covalence</h1>
-						<p className="py-4">
+						<h1 className="text-2xl font-bold text-blue-500 lg:text-5xl">Covalence</h1>
+						<p className="py-2 text-sm lg:text-base lg:py-3">
 							It's a busy day at the Covalence Intelligence Agency (CIA), and there
 							are spies.
 						</p>
-						<p className="py-4">
+						<p className="py-2 text-sm lg:text-base lg:py-3">
 							<span className="font-semibold">YOU</span> are a{' '}
 							<span className="italic">HACKER</span>, an actor in the 5D chess game
 							between government agencies, Anonymous, and hacktivist groups unfolding
@@ -24,12 +24,12 @@ const Home = () => {
 							Employ keyloggers, cryptoanalytic attacks, and botnets to take
 							everything you can and leave before you are discovered... or worse.
 						</p>
-						<p className="py-4">
+						<p className="py-2 text-sm lg:text-base lg:py-3">
 							It's you vs. everyone else. Can you evade security? Fool a polygraph
 							test? Undermine the enemy hackers? Cover your tracks or get got in...
 						</p>
 						<h2
-							className="py-4 text-4xl font-semibold tracking-widest text-indigo-500"
+							className="py-2 text-2xl font-semibold tracking-widest text-indigo-500 lg:text-4xl lg:py-3"
 							id="game-text">
 							PHISHION
 						</h2>
