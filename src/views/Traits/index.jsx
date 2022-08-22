9df25352c60704485b278c71d5649e05f3bb9362
Luke Przkwas <luke@covalence.io>
2022-08-22 12:04:24 -0500

    adding trait filter

--- src/views/Traits/index.jsx
+++ src/views/Traits/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectTrait, refundTrait } from '../../features/traits/traitsSlice';
 
@@ -5,12 +6,47 @@ import { Toast } from '../../components';
 import { GiSave, GiCancel } from 'react-icons/gi';
 
 import { traitsArray } from '../../assets/traits';
+import { skillsMap } from '../../assets/keyMaps';
 
 const Traits = () => {
 	const dispatch = useDispatch();
 	const traitsCurrent = useSelector(state => state.traits.points.current);
 	const selectedTraits = useSelector(state => state.traits.selected);
 	const attributes = useSelector(state => state.attributes);
+
+	const [filter, setFilter] = useState('none');
+	const handleFilterChange = e => setFilter(e.target.value);
+
+	const generateOptions = () => {
+		return Object.keys(skillsMap).map(key => {
+			switch (skillsMap[key].name) {
+				default:
+					return {
+						display:
+							skillsMap[key].name.slice(0, 1).toUpperCase() +
+							skillsMap[key].name.slice(1),
+						key: skillsMap[key].name
+					};
+			}
+		});
+	};
+
+	const handleFilters = () => {
+		if (filter === 'none') return traitsArray;
+
+		const filtered = traitsArray.filter(trait => {
+			if (trait.bonuses.length) {
+				for (const bonus of trait.bonuses) {
+					if (filter === bonus.pair.name && Math.sign(bonus.value) !== -1) {
+						return trait;
+					}
+				}
+			}
+			return false;
+		});
+
+		return filtered;
+	};
 
 	const handleSaveClick = clickedTrait => {
 		if (traitsCurrent === 0) {
@@ -46,8 +82,23 @@ const Traits = () => {
 			<div className="mb-5 text-4xl font-extrabold text-center" id="game-text">
 				Traits
 			</div>
+			<div className="font-mono divider">Filters</div>
+			<section className="w-full px-4 mx-auto lg:w-1/4">
+				<select
+					className="w-full select select-bordered"
+					value={filter}
+					onChange={handleFilterChange}>
+					<option value="none">No Filter</option>
+					{generateOptions().map(option => (
+						<option key={option.key} value={option.key}>
+							{option.display}
+						</option>
+					))}
+				</select>
+			</section>
+			<div className="divider"></div>
 			<section className="flex flex-wrap items-stretch justify-center w-full overflow-hidden md:-mx-3">
-				{traitsArray.map(trait => (
+				{handleFilters().map(trait => (
 					<div
 						key={trait.id}
 						className="w-full my-2 overflow-hidden md:w-1/3 md:my-3 md:px-3">
